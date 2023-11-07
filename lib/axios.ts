9de9675a79655d68052f243cbcba318b909b85e1
Clément Roig <clm.roig@gmail.com>
2023-11-07 12:16:08 +0100

    fix(apiUrl)

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, isAxiosError } from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api",
   headers: {
     "Content-type": "application/json",
   },
