6ad6da2cdf98caa9a0bd3db7178e9f2e3728af9c
Clément Roig <clm.roig@gmail.com>
2023-11-10 00:52:05 +0100

    fix(userWebService): fix error handling

--- domain/User/User.webService.ts
+++ domain/User/User.webService.ts
@@ -1,11 +1,5 @@
-import { ERROR_NAMES, updatePasswordErrors } from "@/domain/User/errors";
 import api, { getErrorMessage } from "@/lib/axios";
 import { User } from "@prisma/client";
-
-const getError = (errorName: ERROR_NAMES) => {
-  const error = updatePasswordErrors.find((e) => e.name === errorName);
-  return error;
-};
 
 export const getUsers = async (): Promise<User[]> => {
   try {
@@ -31,11 +25,8 @@ export const updatePassword = async (
       previousPassword: values.previousPassword,
     });
   } catch (error) {
-    if (error?.response?.data?.name) {
-      const errorName = error?.response?.data?.name;
-      if (Object.values(ERROR_NAMES).includes(errorName)) {
-        throw new Error(getError(errorName as ERROR_NAMES)?.frMessage);
-      }
+    if (error?.response?.data?.frMessage) {
+      throw new Error(error?.response?.data?.frMessage);
     }
     throw new Error(getErrorMessage(error));
   }
