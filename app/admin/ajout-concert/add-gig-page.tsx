160a617021268c76f0f06a2caeeecb8b73011a26
Clément Roig <clm.roig@gmail.com>
2024-01-14 07:48:01 +0100

    refactor(addGig): use useMutation hook instead of custom handleOnSubmit

--- app/admin/ajout-concert/add-gig-page.tsx
+++ app/admin/ajout-concert/add-gig-page.tsx
@@ -3,45 +3,37 @@
 import GigForm from "@/components/GigForm";
 import { CreateGigArgs, createGig } from "@/domain/Gig/Gig.webService";
 import { Box } from "@mantine/core";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
 import { notifications } from "@mantine/notifications";
 import { useRouter } from "next/navigation";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function AddGig() {
-  const { data: session } = useSession();
   const router = useRouter();
   const queryClient = useQueryClient();
-  const [isLoading, setIsLoading] = useState(false);
 
-  const handleOnSubmit = async (values: CreateGigArgs) => {
-    setIsLoading(true);
-    const { user } = session || {};
-    if (user && values) {
-      try {
-        await createGig(values);
-        notifications.show({
-          color: "green",
-          message: "Concert ajouté avec succès !",
-        });
-        await queryClient.invalidateQueries({ queryKey: ["gigs"] });
-        router.push(`/admin`);
-      } catch (error) {
-        notifications.show({
-          color: "red",
-          title: "Erreur à la création d'un concert",
-          message: error.message,
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  };
+  const { isPending, mutate } = useMutation({
+    mutationFn: async (values: CreateGigArgs) => {
+      await createGig(values);
+    },
+    onError: (error) =>
+      notifications.show({
+        color: "red",
+        title: "Erreur à la création du concert",
+        message: error.message,
+      }),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["gigs"] });
+      notifications.show({
+        color: "green",
+        message: "Concert ajouté avec succès !",
+      });
+      router.push(`/admin`);
+    },
+  });
 
   return (
     <Box w={750}>
-      <GigForm isLoading={isLoading} onSubmit={handleOnSubmit} />
+      <GigForm isLoading={isPending} onSubmit={mutate} />
     </Box>
   );
 }
