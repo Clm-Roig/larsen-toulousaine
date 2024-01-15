11bd52d3a207ceec3c7a733c65032bf9c5adb049
Clément Roig <clm.roig@gmail.com>
2024-01-15 11:28:06 +0100

    feat(thisWeekGigs): add dynamic metadata

--- app/cette-semaine/page.tsx
+++ app/cette-semaine/page.tsx
@@ -3,11 +3,34 @@ import Layout from "@/components/Layout";
 import { Suspense } from "react";
 import WeekGigs from "./WeekGigs";
 import { Metadata } from "next";
+import { getGigs } from "@/domain/Gig/Gig.webService";
+import dayjs from "dayjs";
+import { getGigTitleFromGigSlug } from "@/domain/Gig/Gig.service";
 
-export const metadata: Metadata = {
-  title: "Concerts de la semaine",
-  description: "Les concerts metal de la semaine sur Toulouse",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "Concerts metal de la semaine à Toulouse";
+  const gigs = await getGigs(
+    dayjs().startOf("week").toDate(),
+    dayjs().endOf("week").toDate(),
+  );
+  const images: string[] = gigs.reduce((images, gig) => {
+    if (!gig.imageUrl) return images;
+    return [...images, gig.imageUrl];
+  }, []);
+  const description: string = gigs
+    .map((gig) => getGigTitleFromGigSlug(gig.slug))
+    .join("\n");
+  return {
+    title: title,
+    description,
+    assets: images,
+    openGraph: {
+      images: images,
+      title: title,
+      description,
+    },
+  };
+}
 
 export default function Page() {
   return (
