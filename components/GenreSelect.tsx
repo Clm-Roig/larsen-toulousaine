853914e7e4d523cde12b65fdce0d0816ce33ffbc
Clément Roig <clm.roig@gmail.com>
2024-10-05 22:38:08 +0200

    feat(genreSelect): make filtering case unsensitive

--- components/GenreSelect.tsx
+++ components/GenreSelect.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { MultiSelect, MultiSelectProps } from "@mantine/core";
+import { normalizeString } from "@/utils/utils";
+import {
+  ComboboxItem,
+  MultiSelect,
+  MultiSelectProps,
+  OptionsFilter,
+} from "@mantine/core";
 import { Genre } from "@prisma/client";
 
 export type GenreSelectProps = {
@@ -11,6 +17,18 @@ export default function GenreSelect({
   genres,
   ...selectProps
 }: GenreSelectProps) {
+  // Case unsensitive filtering
+  const optionsFilter: OptionsFilter = ({ options, search }) => {
+    const filtered = (options as ComboboxItem[]).filter(
+      (option: ComboboxItem) =>
+        normalizeString(option.label)
+          .trim()
+          .includes(normalizeString(search).trim()),
+    );
+    filtered.sort((a, b) => a.label.localeCompare(b.label));
+    return filtered;
+  };
+
   return (
     <MultiSelect
       searchable
@@ -18,6 +36,7 @@ export default function GenreSelect({
         value: genre.id,
         label: genre.name,
       }))}
+      filter={optionsFilter}
       {...selectProps}
     />
   );
