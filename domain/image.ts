cf9f1023d7ab9cd579fe09ee286d492c5f6234fe
Clément Roig <clm.roig@gmail.com>
2025-01-15 20:16:12 +0100

    fix(gigImg): decrease max image size

--- domain/image.ts
+++ domain/image.ts
@@ -1,6 +1,6 @@
 export const GIG_IMG_RATIO = 16 / 9;
 export const GIG_IMG_RATIO_STRING = "16/9";
-export const MAX_IMAGE_SIZE = 5 * 1000000; // 5Mo
+export const MAX_IMAGE_SIZE = 3 * 1000000; // 3Mo
 
 export const getGigImgHeight = (width: number): number => width / GIG_IMG_RATIO;
 export const getGigImgWidth = (height: number): number =>
