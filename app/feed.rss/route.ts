ec6bad3c59ff63533bf926d57f6f8fd066bae3d2
Clément Roig <clm.roig@gmail.com>
2025-09-04 16:40:04 +0200

    fix(dayjsConfig): use configured dayjs everywhere

--- app/feed.rss/route.ts
+++ app/feed.rss/route.ts
@@ -41,8 +41,8 @@ export async function GET() {
   cleanedGigs.map((gig) => {
     feed.item({
       title: gig.name
-        ? `${gig.name} ${dayjs.tz(gig.date).format("YYYY")}`
-        : `${dayjs.tz(gig.date).format("DD/MM/YYYY")} : ${getBandNames(
+        ? `${gig.name} ${dayjs(gig.date).format("YYYY")}`
+        : `${dayjs(gig.date).format("DD/MM/YYYY")} : ${getBandNames(
             gig.bands,
           )}`,
       date: gig.createdAt,
