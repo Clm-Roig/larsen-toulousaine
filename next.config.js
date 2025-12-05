202b73cf8474409a95c452d39f7aa4fb888c7a9b
Clément Roig <clm.roig@gmail.com>
2025-12-05 16:30:21 +0100

    tech(deploy): fix netlify error with prisma

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
   },
 
   turbopack: {},
+
+  netlify: {
+    edgeMiddleware: false, // prevent Netlify from deploying prisma in Edge Middleware
+  },
 };
 
 module.exports = withPWA(nextConfig);
