e7155a7f791691ac80312e1e37a23c06b20ac8be
Clément Roig <clm.roig@gmail.com>
2025-12-17 20:09:40 +0100

    improve performance by SSR places & genres

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ import { Suspense } from "react";
 import Gigs from "@/app/gigs";
 import { getMetadata } from "@/utils/metadata";
 import { Metadata } from "next";
+import { getGenres } from "@/domain/Genre/Genre.webService";
+import { getPlaces } from "@/domain/Place/Place.webService";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+export const revalidate = 3600; // Cache cette page 1h
 
 export function generateMetadata(): Metadata {
   const title = "Larsen Toulousaine, l'agenda des évènements metal à Toulouse";
@@ -21,23 +30,39 @@ export function generateMetadata(): Metadata {
   );
 }
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient();
+
+  // SSR prefetching
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ["genres"],
+      queryFn: getGenres,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ["places"],
+      queryFn: getPlaces,
+    }),
+  ]);
+
   return (
-    <Layout>
-      <Box>
-        <Title order={1} className="visually-hidden-seo-friendly">
-          Concerts et festivals metal à Toulouse
-        </Title>
-        <Suspense
-          fallback={
-            <Center h={200}>
-              <Loader />
-            </Center>
-          }
-        >
-          <Gigs />
-        </Suspense>
-      </Box>
-    </Layout>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Layout>
+        <Box>
+          <Title order={1} className="visually-hidden-seo-friendly">
+            Concerts et festivals metal à Toulouse
+          </Title>
+          <Suspense
+            fallback={
+              <Center h={200}>
+                <Loader />
+              </Center>
+            }
+          >
+            <Gigs />
+          </Suspense>
+        </Box>
+      </Layout>
+    </HydrationBoundary>
   );
 }
