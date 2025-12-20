81c55dc6582dc67028eba9f9d398a329a3fd84c8
Clément Roig <clm.roig@gmail.com>
2025-12-20 13:05:55 +0100

    fix(gigs): fix gigs list when coming back from a gig in the future

--- app/gigs.tsx
+++ app/gigs.tsx
@@ -2,7 +2,7 @@
 import { Genre, Place } from "@prisma/client";
 import GigList from "@/components/GigList";
 import useMonthGigs from "@/hooks/useMonthGigs";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getGenres } from "@/domain/Genre/Genre.webService";
 import { getPlaces } from "@/domain/Place/Place.webService";
@@ -25,19 +25,8 @@ export default function Gigs() {
     staleTime: 60 * 60 * 5, // 5min
   });
 
-  const selectedMonth = useMemo(() => {
-    const year = searchParams.get("année");
-    const monthNb = searchParams.get("mois");
-
-    if (year && monthNb) {
-      return new Date(Number(year), Number(monthNb) - 1, 1);
-    }
-
-    const now = new Date();
-    return new Date(now.getFullYear(), now.getMonth(), 1);
-  }, [searchParams]);
-
-  const { isLoading, monthGigs, setSelectedMonth } = useMonthGigs();
+  const { isLoading, monthGigs, setSelectedMonth, selectedMonth } =
+    useMonthGigs();
 
   const filteredPlaces = useMemo(
     () => places.filter((p) => displayNotSafePlaces || p.isSafe),
@@ -48,10 +37,22 @@ export default function Gigs() {
     setSelectedMonth(newMonth);
     const changes = new Map<string, number>([
       ["année", newMonth.getFullYear()],
-      ["mois", newMonth.getMonth() + 1],
+      ["mois", newMonth.getMonth() + 1], // getMonth() goes from 0 to 11
     ]);
     setSearchParams(changes);
   };
+
+  useEffect(() => {
+    const year = searchParams.get("année");
+    const monthNb = searchParams.get("mois");
+
+    if (year && monthNb) {
+      setSelectedMonth(new Date(Number(year), Number(monthNb) - 1, 1));
+    } else {
+      const now = new Date();
+      setSelectedMonth(new Date(now.getFullYear(), now.getMonth(), 1));
+    }
+  }, [searchParams, setSelectedMonth]);
 
   return (
     <GigList
